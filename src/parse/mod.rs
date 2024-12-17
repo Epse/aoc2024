11849d55e6d5be0b31dc5d6c1fa40f64eeeb271a
Stef Pletinck <stef.pletinck@ugent.be>
2024-12-17 12:18:39 +0100

    Add matrix parsing

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -18,6 +18,28 @@ pub fn parse_two_i32_lists(path: &Path) -> Result<(Vec<i32>, Vec<i32>), String> 
     Ok(lines)
 }
 
+fn parse_matrix<T>(path: &Path) -> Result<Vec<Vec<T>>, String>
+    where T: std::str::FromStr
+{
+    let file = File::open(path).expect("Requires datafile");
+    let lines = io::BufReader::new(file)
+        .lines()
+        .filter_map(|line| {
+            if line.is_err() {
+                None
+            } else {
+                Some(
+                    // Unwrap not needed
+                    line.unwrap().split(" ")
+                        .filter_map(|x| x.parse::<T>().ok())
+                        .collect::<Vec<T>>()
+                )
+            }
+        })
+        .collect::<Vec<Vec<T>>>();
+    Ok(lines)
+}
+
 fn split_line_to_num(line: &String) -> Option<(i32, i32)> {
     let split = line.split(" ").collect::<Vec<&str>>();
     if split.len() < 2 {
