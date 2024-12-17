8db4f9462ecd6ac4f5434f964057abc629bb6baf
Stef Pletinck <stef.pletinck@ugent.be>
2024-12-17 14:06:10 +0100

    Solve day three part two

--- src/three.rs
+++ src/three.rs
@@ -8,6 +8,7 @@ pub fn run() {
     file.read_to_string(&mut input).expect("Need to read input");
 
     println!("Mult: {}", do_mul(&input));
+    println!("Conditional (lazy): {}", do_conditional(&input));
 }
 
 fn do_mul(input: &str) -> i64 {
@@ -18,6 +19,23 @@ fn do_mul(input: &str) -> i64 {
     }).sum()
 }
 
+fn do_conditional(input: &str) -> i64 {
+    let mut enabled = true;
+    let mut sum: i64 = 0;
+    for elem in input.split("do") {
+        if elem.starts_with("n't") {
+            enabled = false
+        }
+        if elem.starts_with("()") {
+            enabled = true
+        }
+        if enabled {
+            sum += do_mul(&elem);
+        }
+    }
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +44,11 @@ mod tests {
     fn example_input() {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(do_mul(input), 161);
+    }
+
+    #[test]
+    fn conditional_example() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(do_conditional(&input), 48);
     }
 }
